8dbb240d8c2933d822f5a0e99284e2bbb04fa885
shabatel <48761060+shabatel@users.noreply.github.com>
2021-01-05 21:15:24 +0200

    adding mongoDB

--- exchange-service/src/main/java/com/example/demo/ExchangeController.java
+++ exchange-service/src/main/java/com/example/demo/ExchangeController.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
+import com.mongodb.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExchangeController {
+
+    MongoClient mongoClient;
+    DB database;
+    DBCollection collection;
+
+
     @GetMapping("/currexg/from/{from}/to/{to}")
     public ExchangeVal getInventoryDetails(@PathVariable("from") Currencies from, @PathVariable("to") Currencies to) {
         ExchangeVal exgVal = null;
+
+        initialAndPopulateDB();
+
+        DBObject query = new BasicDBObject("_id", from);
+        DBCursor cursor = collection.find(query);
+
+        DBObject jo = cursor.one();
+
+        System.out.println((String)cursor.one().get("rate"));
+
 
         if (Currencies.USD == from && Currencies.ILS == to) {
             exgVal = new ExchangeVal(901, from, to, 6);
@@ -17,4 +34,16 @@ public class ExchangeController {
         }
             return exgVal;
         }
+
+    private void initialAndPopulateDB() {
+        mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        database = mongoClient.getDB("example");
+        collection = database.getCollection("currencies");
+
+        collection.insert(new BasicDBObject("_id", "USD").append("rate", "1"));
+        collection.insert(new BasicDBObject("_id", "ILS").append("rate", "3.203"));
+        collection.insert(new BasicDBObject("_id", "EUR").append("rate", "0.813"));
+        collection.insert(new BasicDBObject("_id", "GBP").append("rate", "0.734"));
+        collection.insert(new BasicDBObject("_id", "INR").append("rate", "73.126"));
     }
+}
---
+++ exchange-service/src/main/java/com/example/demo/mongoDBHandler.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+public class mongoDBHandler {
+
+    public static MongoClient getMongoClient() {
+//        return new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        return null;
+    }
+}
